34953c80c272b470114b889c607230ffc0b06160
Gobierno Autonomo Oruro <gobiernooruro2024@gmail.com>
2024-10-01 17:01:58 -0400

    fix .gitignore, dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,7 @@ FROM node:16-alpine
 
 WORKDIR /usr/src/app
 
-COPY package*.json ./
-COPY prisma ./prisma/ 
+COPY package*.json ./ 
 
 RUN npm install
 
--- init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Inicializa la base de datos si no está ya inicializada
-if [ ! -s "$PGDATA/PG_VERSION" ]; then
-    initdb -D "$PGDATA" --username=$POSTGRES_USER --pwfile=<(echo $POSTGRES_PASSWORD)
-fi
-
-# Inicia el servidor PostgreSQL
-exec postgres -D "$PGDATA"
