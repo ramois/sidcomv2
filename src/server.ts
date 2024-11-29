8e145aae3b6c9ea5dc978a0ee32dd35c325987e7
Gobierno Autonomo Oruro <gobiernooruro2024@gmail.com>
2024-11-29 18:40:01 -0400

    fix server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
-/*import app from './app'
+import app from './app'
 
 const PORT = process.env.PORT
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`)
-})*/
+})
 //el de arriba es el oficlal
 
-import app from './app'
+/* import app from './app'
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000; // 3000 como valor por defecto
 //const PORT = process.env.PORT; // Define un puerto por defecto
@@ -15,4 +15,4 @@ const HOST = '192.168.130.128'; // Define la IP en la que quieres que escuche
 
 app.listen(PORT, HOST, () => {
     console.log(`Server is running on ${HOST}:${PORT}`);
-});
+}); */
